04c8f19a13016f89ce217ecd93de6c3462621808
Maks <Crossfade143@gmail.com>
2021-03-20 22:53:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require('./app')
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.end(`Server STARTED ${PORT}`))
+
+app.listen(PORT, () => console.log('Server started'))
